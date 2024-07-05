1a80203c56f27d3b670c1f442ff0a124f2addba4
Martha Pruitt <martha.is.a.pruitt@gmail.com>
2024-07-04 20:32:40 -0500

    it ticks

--- clock.js
+++ clock.js
@@ -1,20 +1,20 @@
 const body = document.querySelector("body"),
     hourHand = document.querySelector(".hour"),
-    minuteHand = document.querySelector("minute"),
-    secondHand = document.querySelector("second"),
+    minuteHand = document.querySelector(".minute"),
+    secondHand = document.querySelector(".second"),
     modeSwitch = document.querySelector(".modeSwitch");
 modeSwitch.addEventListener("click",() =>{
-    body.classList.toggle("dark")
-})
+    body.classList.toggle("dark");
+});
 const updateTime=()=>{
-    let date=new Date()
+    let date=new Date();
     secToDeg = (date.getSeconds()/60) * 360;
     minToDeg = (date.getMinutes()/60) *360;
     hrToDeg = (date.getHours()/60)*360;
-   // console.log(secToDeg)
-   secondHand.style.transform=`rotate(${secToDeg})`;
-   minuteHand.style.transform=`rotate(${minToDeg})`
-   hourHand.style.transform=`rotate(${hrToDeg})`
-}
+    console.log(`updateTime ${minToDeg} ${secToDeg}`);
+   secondHand.style.transform=`rotate(${secToDeg}deg)`;
+   minuteHand.style.transform=`rotate(${minToDeg}deg)`;
+   hourHand.style.transform=`rotate(${hrToDeg}deg)`;
+};
 setInterval(updateTime,1000);
 updateTime();
